94efde7d5878c9527ee166f1efdb764541d2412c
MinkyungJ <126550894+MinkyungJ@users.noreply.github.com>
2023-04-04 21:28:26 +0900

    #14 feat GET,POST, PATCH, DELETE 기능추가

--- server/routes/basket/basket.js
+++ server/routes/basket/basket.js
@@ -1,26 +1,64 @@
 'use strict'
 
 module.exports =  async function getBasket(fastify, opts) {
-  fastify.get('/', async function (req, res) {
-    const getSchema = (
-      {
-        "baskets": [
-          {
-            "id": 1,
-            "product": {
-              "id": 1,
-              "category": "잡화",
-              "seller": "김코딩",
-              "name": "물티슈",
-              "price": 1000,
-              "brand": "노브랜드",
-              "status": "판매중"
-            },
-            "quantity": 1
-          }
-        ]
+  fastify.get('/', async function (request, reply) {
+    console.log(request.headers.userid);
+    const client = await fastify.pg.connect()
+    try {
+      const { rows } = await client.query(
+        `SELECT public.basket.id, public.user.username, public.product.brand, public.product.name, public.product.price, public.category.name, public.basket.quantity
+          FROM public.basket
+          INNER JOIN public.user ON public.basket.user_id = public.user.id
+          INNER JOIN public.product ON public.basket.product_id = public.product.id
+          INNER JOIN public.category ON public.product.category_id = public.category.id
+        WHERE public.basket.user_id = $1`,
+        [request.headers.userid]
+        )
+        reply.code(200).send(rows)
+        } finally {
+          client.release()
+        }
       }
-    )
-    return getSchema;
+    ),
+  
+  fastify.post('/', async function (request, reply) {
+    const user_id = request.body.user_id
+    const product_id = request.body.product_id
+    const quantity = request.body.quantity
+    const client = await fastify.pg.connect()
+    try {
+      const { rows } = await client.query(
+        'INSERT INTO public.basket(user_id, product_id, quantity) VALUES($1, $2, $3)', [user_id, product_id, quantity]
+      )
+      reply.code(201).send(rows)
+    } finally {
+      client.release()
+    }
+  }),
+
+  fastify.patch('/:id', async function (request, reply) {
+    const quantity = request.body.quantity
+    const client = await fastify.pg.connect()
+    try{
+      const { rows } = await client.query(
+        'UPDATE public.basket SET quantity = $1 WHERE id = $2', [quantity, request.params.id]
+      )
+      reply.code(200).send(rows)
+    } finally {
+      client.release()
+    }
+  }),
+
+  fastify.delete('/', async function (request, reply) {
+    const client = await fastify.pg.connect()
+    try{
+      const { rows } = await client.query(
+        'DELETE FROM public.basket WHERE id = $1', [request.body.id]
+      )
+      reply.code(200).send(rows)
+    } finally {
+      client.release()
+    }
   })
 }
+
