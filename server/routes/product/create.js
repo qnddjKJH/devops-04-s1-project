816d6836e39b48be32f806a7890624b681d2345d
doin-doin <ineemi@hanmail.net>
2023-04-04 21:39:13 +0900

    #13

--- server/routes/product/create.js
+++ server/routes/product/create.js
@@ -1,22 +1,23 @@
 'use strict'
 
-//const { createOne, isValid } = require('./product')
-const { createOne } = require('./product')
-
 module.exports = async function (fastify, opts) {
-  fastify.post('/', async function (request, reply) {
-    // if(!isValid(request.body)) {
-    //   reply
-    //     .code(400)
-    //     .header('Content-type','text/plain')
-    //     .send('Bad Request')
-    // }
+    fastify.post('/', async function (request, reply) {
+        const client = await fastify.pg.connect();
+        try{
+            const {status,name,price,brand,category_id,user_id} = request.body
 
-    const result = await createOne(request.body)
-    
-    reply
-      .code(200)
-      .header('Content-type','application/json')
-      .send(result)
-  })
+            const {rows} = await client.query(
+                `INSERT INTO product (status,name,price,brand,category_id,user_id) VALUES ($1,$2,$3,$4,$5,$6)`,[status,name,price,brand,category_id,user_id]
+            )
+
+            if(rows) {
+                reply
+                .code(200)
+                .header('Content-type','application/json')
+                .send(rows)
+            }
+        }finally {
+            client.release();
+        }
+    })
 }
